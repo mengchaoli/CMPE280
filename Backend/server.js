31a48bbd55c8b42d164c4a89eee01917b0f4bd6b
Bo Miao <bo.miao@sjsu.edu>
2020-02-16 10:03:37 -0800

    merge from fork

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,32 @@
 var express = require("express");
-var login = require('./routes/loginroutes');
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 var app = express();
+
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+const mongoose = require("mongoose");
+const users = require("./routes/loginroutes");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
+
+app.use("/api/users", users);
+
 var router = express.Router();
 // test route
-router.get('/', function (req, res) {
-    res.json({ message: 'welcome to our upload module apis' });
+router.get("/", function(req, res) {
+  res.json({ message: "welcome to our upload module apis" });
 });
 //route to handle user registration
 //router.post('/register', login.register);
 //router.post('/login', login.login)
-app.use('/api', router);
-app.listen(5000);
+app.use("/api", router);
+app.listen(3000);
