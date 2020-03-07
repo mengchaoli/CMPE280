d4e946a328d1c35976f14896cac69f7513cf30dd
JingWang <jingwang9302@gmail.com>
2020-03-06 16:45:06 -0800

    create jQuery tab and dialog widget && update footer and navBar

--- Backend/data/news.js
+++ Backend/data/news.js
@@ -11,7 +11,7 @@ var news = [
     },
     {
         "title":"Coronavirus Spreads Recession Fears Around The World",
-        "body": "As odds of a global recession rise, governments and central banks around the world are racing to fend off the economic damage from the spread of the coronavirus. The toll has already landed hard on jittery financial markets. Stocks continued to sell off on Thursday as the Dow Jones Industrial Average plunged 969 points, or about 3.6%, as investors fled stocks. Companies have shut factories, canceled conferences and drastically scaled back employee travel.",
+        "body": "As odds of a global recession rise, governments and central banks around the world are racing to fend off the economic damage from the spread of the coronavirus. ",
         "image": "https://media.npr.org/assets/img/2020/03/05/gettyimages-1203659276-5c38a81c7843d456db7f94268e8f3e21ed667f99-s1600-c85.jpg"
     },
     {
@@ -26,9 +26,29 @@ var news = [
     },
     {
         "title":"1Coronavirus Spreads Recession Fears Around The World",
-        "body": "As odds of a global recession rise, governments and central banks around the world are racing to fend off the economic damage from the spread of the coronavirus. The toll has already landed hard on jittery financial markets. Stocks continued to sell off on Thursday as the Dow Jones Industrial Average plunged 969 points, or about 3.6%, as investors fled stocks. Companies have shut factories, canceled conferences and drastically scaled back employee travel.",
+        "body": "As odds of a global recession rise, governments and central banks around the world are racing to fend off the economic damage from the spread of the coronavirus. The toll has already landed hard on jittery financial markets.",
         "image": "https://media.npr.org/assets/img/2020/03/05/gettyimages-1203659276-5c38a81c7843d456db7f94268e8f3e21ed667f99-s1600-c85.jpg"
-    }
+    },
+    {
+        "title":"Maryland Confirms 3 Coronavirus Cases, As COVID-19 Spreads In Other States",
+        "body": "As COVID-19 spreads in the U.S., Maryland health officials are the latest to announce that a handful of Montgomery County residents have tested positive for the virus that causes it.",
+        "image": "https://media.npr.org/assets/img/2020/03/05/ap_20036659257981-c9cf650db13aed4ef699f98c6f46557403586198-s1600-c85.jpg"
+    },
+    {
+        "title":"How Coronavirus Spreads: A Cough In Your Face ... Or A Kiss On Your Cheek",
+        "body": "Keep your distance. And don't kiss.Those are two pieces of advice that could be crucial in reducing the spread of the coronavirus. Public health officials say the spread has been mainly driven through people spending time indoors with others who have the disease.",
+        "image": "https://media.npr.org/assets/img/2020/03/05/gettyimages-1203659276-5c38a81c7843d456db7f94268e8f3e21ed667f99-s1600-c85.jpg"
+    },
+    {
+        "title":"Coronavirus Spreads Recession Fears Around The World",
+        "body": "As odds of a global recession rise, governments and central banks around the world are racing to fend off the economic damage from the spread of the coronavirus. ",
+        "image": "https://media.npr.org/assets/img/2020/03/05/gettyimages-1203659276-5c38a81c7843d456db7f94268e8f3e21ed667f99-s1600-c85.jpg"
+    },
+    {
+        "title":"1Maryland Confirms 3 Coronavirus Cases, As COVID-19 Spreads In Other States",
+        "body": "As COVID-19 spreads in the U.S., Maryland health officials are the latest to announce that a handful of Montgomery County residents have tested positive for the virus that causes it.",
+        "image": "https://media.npr.org/assets/img/2020/03/05/ap_20036659257981-c9cf650db13aed4ef699f98c6f46557403586198-s1600-c85.jpg"
+    },
 ]
 
 module.exports = news;
--- Backend/public/newsPage.js
+++ Backend/public/newsPage.js
@@ -1,13 +1,40 @@
-function doAjax(e) {
-    $(".card").attr("style", "display: block;");
-    var id = e.target.id;
-    var query = "?id=" + id;
-    $.get('/api/news' + query, function (news) {
-        $("#newsTitle").html(news.title);
-        $("#newsBody").html(news.body);
-        $("#newsImage").attr("src",news.image);
-    });
+// // AJAX show tab (not the jquery widget version)
+// function doAjax(e) {
+//     $(".card").attr("style", "display: block;");
+//     var id = e.target.id;
+//     var query = "?id=" + id;
+//     $.get('/api/news' + query, function (news) {
+//         $("#newsTitle").html(news.title);
+//         $("#newsBody").html(news.body);
+//         $("#newsImage").attr("src",news.image);
+//     });
+// }
+// $(".nav-link").click(doAjax)
+// $(".card").attr("style", "display: none;");
+
+//      <li class="nav-item"><a class="nav-link" href="/api/news?id=1">Feb.02</a></li>
+$.get("http://localhost:5000/api/newscount", function (res) {
+    var count = res.count;
+    for (var i = 0; i < count; i++) {
+        var item = document.createElement("li");
+        item.className = "nav-item";
+        var link = document.createElement("a");
+        link.className = "nav-link";
+        link.setAttribute("href", "/api/news?id=" + i);
+        link.innerHTML = "news" + i;
+        item.appendChild(link);
+        document.querySelector("ul.nav.nav-tabs").appendChild(item);
+    }
+    $("#tabs").tabs();
+})
+
+// dialog widget init
+$("#message").dialog({ autoOpen: false });
+
+function openMessage() {
+    $("#message").dialog("open");
 }
 
-$(".nav-link").click(doAjax)
-$(".card").attr("style", "display: none;");
+function closeMessage() {
+    $("#message").dialog("close");
+}
