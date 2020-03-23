0f03737eded85837fb5056760914268bc78d9a29
BoMiao2020 <miaobo201808@gmail.com>
2020-03-22 23:19:21 -0700

    search add delete through mongodb, need to polish (connecting user with our own db)

--- Backend/models/modelMain.js
+++ Backend/models/modelMain.js
@@ -1,8 +1,72 @@
 module.exports.get_userlist = function(req, res) {
   var db = req.db;
-  console.log(db);
+  //   console.log(db);
   var collection = db.get("users");
   collection.find({}, {}, function(err, docs) {
     res.render("userlist", { userlist: docs });
+  });
+};
+
+module.exports.get_showuser = function(req, res) {
+  var uemail = req.params.user_email; // if id should transform string to int
+  var db = req.db;
+  var collection = db.get("users");
+
+  collection.find({ email: uemail }, function(err, doc) {
+    if (err) {
+      res.send("Find failed.");
+    } else {
+      console.log(doc);
+      res.render("showuser", {
+        title: "Show User: " + uemail,
+        mail: doc[0].email
+      });
+    }
+  });
+};
+
+// id: 0,
+// email: "admin@admin.com",
+// password: "admin",
+// first_name: "admin",
+// last_name: "admin"
+module.exports.post_adduser = function(req, res) {
+  var db = req.db;
+
+  var uemail = req.body.email;
+  var upassword = req.body.password;
+  var ufname = req.body.first_name;
+  var ulname = req.body.last_name;
+
+  var collection = db.get("users");
+  collection.insert(
+    {
+      id: 100,
+      email: uemail,
+      password: upassword,
+      first_name: ufname,
+      last_name: ulname
+    },
+    function(err, doc) {
+      if (err) {
+        res.send("Insert failed.");
+      } else {
+        res.redirect("userlist");
+      }
+    }
+  );
+};
+
+module.exports.post_deleteuser = function(req, res) {
+  var uemail = req.params.user_email;
+  var db = req.db;
+  var collection = db.get("users");
+
+  collection.remove({ email: uemail }, function(err, doc) {
+    if (err) {
+      res.send("Delete failed");
+    } else {
+      res.send("Successfully deleted " + uemail);
+    }
   });
 };
--- Backend/routes/dbuserroutes.js
+++ Backend/routes/dbuserroutes.js
@@ -3,5 +3,15 @@ const router = express.Router();
 const modelMain = require("../models/modelMain");
 
 router.get("/userlist", modelMain.get_userlist);
+router.get("/userlist/:user_email", modelMain.get_showuser);
+router.get("/newuser", function(req, res) {
+  res.render("newuser");
+});
+router.post("/adduser", modelMain.post_adduser);
+router.get("/deleteuser/:user_email", function(req, res) {
+  var uemail = req.params.user_email;
+  res.render("deleteuser", { useremail: uemail });
+});
+router.post("/deleteuser/:user_email", modelMain.post_deleteuser);
 
 module.exports = router;
